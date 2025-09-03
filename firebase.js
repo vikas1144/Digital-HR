17e54d303adba2cd4a707c636466ddd7dda5bfec
Vikas Kashyap <146576120+vikas1144@users.noreply.github.com>
2025-09-03 12:02:14 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgl3O_5dBMvB79kjHTWey3e9xNlF_tDIA",
-  authDomain: "digitalhr-2c4c4.firebaseapp.com",
-  projectId: "digitalhr-2c4c4",
-  storageBucket: "digitalhr-2c4c4.firebasestorage.app",
-  messagingSenderId: "513390614700",
-  appId: "1:513390614700:web:d7b11bcfeb2a06f81b2709",
-  measurementId: "G-9RBTNL57WL"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxx",
+  projectId: "xxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,3 +21,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { app,db,auth ,storage};
+
